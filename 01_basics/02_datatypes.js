733fd1cb520d957b5ccb1801a371326f44a71a66
Vibhor <vibhorsahu26@gmail.com>
2026-06-01 11:23:37 +0530

    adding datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+// mostly same as we have studied earlier in c and cpp
+let accountname="vibhor" // string 
+let number=23455 // number data type ranges from 2 to power 52
+// boolean data type= true/false
+// null = standalone data type
+let accoundid=null
+// null is different from undefined 
+// undefined means value is not defined yet but null means null value is defined to variable
+console.log(typeof accoundid); // type of null is object
+let name 
+console.log(typeof name); // type of undefined is undefined itself
