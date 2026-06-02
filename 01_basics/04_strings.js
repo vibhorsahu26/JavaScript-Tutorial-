d6a4e24553abb850ef5944e698e3f680cbf44c95
Vibhor <vibhorsahu26@gmail.com>
2026-06-03 00:11:36 +0530

    adding changes

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -9,6 +9,15 @@ console.log(MyName.slice(4,6));// in slice no matter what is our start parameter
 console.log(MyName.indexOf("z"));// gives index of character of a given string, if char is not present output will be -1
 console.log(MyName.charAt(2)); // prints value of char at that particular index of particular string
 console.log(`Hello my name is ${MyName}, I am learning javascript`); // use to concat string while continuing current line
+console.log(MyName.toLowerCase());// used to lower case string
+console.log(MyName.toUpperCase());// used to upper case string
+console.log(MyName.includes("v")); // gives true or false depending on that particular char present in given string or not
+console.log(MyName.substring(0,4));// gives a substring
+console.log(MyName.endsWith('r'));// return true or false depending on string ends with that particular character or not
+console.log(MyName.startsWith('i'));//  return true or false depending on string starts with that particular character or not
+console.log(MyName.repeat(3));// repeat a string to n given times
+console.log(MyName.split(""));// string to array
+
 
 
 
