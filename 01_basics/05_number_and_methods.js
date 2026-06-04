65d1c3b6f4909830da46af341d768533baec9da1
Vibhor <vibhorsahu26@gmail.com>
2026-06-04 19:43:14 +0530

    adding number and methods

---
+++ 01_basics/05_number_and_methods.js
@@ -0,0 +1,30 @@
+let AccountNum=2345678987654n
+console.log(AccountNum);
+console.log(typeof AccountNum);
+let Array= [1,3,5,6,7]
+console.log(Array);
+console.log(Array.toString());
+let accountString=(AccountNum.toString());// number to string
+console.log(accountString);
+console.log(typeof accountString);
+let num=13.45667
+console.log(num.toFixed(2)); // round of decimal digits till the given number
+console.log(num.toPrecision(3));// round of the whole number in given digits
+console.log(num.toExponential(3));// gives power of n to given time
+console.log(num)
+console.log(Math.random());// gives value between 0 to 1
+console.log(Math.pow(2,3)); // gives 2***3
+console.log(Math.max(1,3,4)); // gives max value
+console.log(Math.min(1,5,0));// gives min value
+console.log(Math.floor(4,65)); // gives floor value
+
+
+
+
+
+
+
+
+
+
+
