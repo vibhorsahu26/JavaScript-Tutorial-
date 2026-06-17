850d29e3f21a9ad5d3d84c48619c43591ff8bf56
Vibhor <vibhorsahu26@gmail.com>
2026-06-17 12:57:25 +0530

    added for each

--- 05_iterations/05_foreach.js
+++ 05_iterations/05_foreach.js
@@ -16,3 +16,27 @@ function print(anyarray){
     
 }
 MyArray.forEach(print) // using this way we can also use for each
+
+MyArray.forEach((item,index)=>{
+    console.log(item , index); // pritnts value and its index
+    
+})
+
+const info = [
+    {
+        name:"vibhor",
+        email:"v@gmail.com"
+    },
+    {
+        name:"random",
+        email:"r@gmail.com"
+    },
+    {
+        name:"person",
+        email:"p@gmail.com"
+    }
+]
+
+info.forEach((item)=>{
+    console.log(item.name);
+})
