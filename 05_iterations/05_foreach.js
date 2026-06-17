c9048acba12a684dc54246decdf33b2e5fa67e65
Vibhor <vibhorsahu26@gmail.com>
2026-06-17 12:46:06 +0530

    added for each

---
+++ 05_iterations/05_foreach.js
@@ -0,0 +1,18 @@
+// for each
+// in this we form a function of anything which we want to perform 
+// function can be a normal named function or arrow function
+
+let MyArray = [1,2,3,4,5]
+// MyArray.forEach( function printme(MyArray) {
+//     console.log(MyArray);
+// })
+
+MyArray.forEach((MyArray)=>{ // here we have created a arrow function
+    console.log(MyArray); 
+})
+
+function print(anyarray){
+    console.log(anyarray);
+    
+}
+MyArray.forEach(print) // using this way we can also use for each
